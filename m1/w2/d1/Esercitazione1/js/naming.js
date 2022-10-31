9698602b0a236c30b21011d53939e6c88f80b95c
VanessaOteng <vanessaoteng52@gmail.com>
2022-10-31 11:34:55 +0100

    esetcitazione-w2-d1

---
+++ m1/w2/d1/Esercitazione1/js/naming.js
@@ -0,0 +1,19 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'il criceto';
+var PetPreferito = 'il coniglio';
+
+var $pet = 'la giraffa';
+console.log($pet);
+var _pet = 'il leone';
+console.log(_pet);
+var _pet2 = 'la pantera';
+console.log(_pet2);
+
+
+
+
+//var 12 = 'numero';
---
+++ m1/w2/d1/script.js
@@ -0,0 +1,21 @@
+/*
+Commenti su piu righe
+*/
+//commento di 1 riga
+
+/*
+Direttive di output
+*/
+
+
+document.querySelector('#test').innerHTML = 'Ciao'
+/*alert('mostra questa scritta');*/
+console.log('Mostra questa scritta');// strumento di debuging
+
+console.log(3 + 2);
+
+console.log('%c Mostra questa scritta', 'color: red; background-color:blue');
+
+document.getElementById('test').innerHTML = '<b>Prova</b>';
+
+document.write('Ciao')//Elimina TUTTO cio che lo precede, va utilizzato poco
